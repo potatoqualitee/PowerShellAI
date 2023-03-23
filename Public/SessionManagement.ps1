29f4b7a81bd9147f88322f1caa15feb5fecdc6cf
dfinke <finked@hotmail.com>
2023-03-23 12:05:23 -0400

    flesh out session file functions

---
+++ Public/SessionManagement.ps1
@@ -0,0 +1,44 @@
+$Script:timeStamp
+
+function Get-ChatSessionTimeStamp {
+    [CmdletBinding()]
+    param ()
+
+    if ($null -eq $Script:timeStamp) {
+        $Script:timeStamp = (Get-Date).ToString("yyyyMMddHHmmss")
+    }
+
+    $Script:timeStamp
+}
+
+function Reset-ChatSessionTimeStamp {
+    [CmdletBinding()]
+    param ()
+
+    $Script:timeStamp = $null
+}
+
+function Get-ChatSessionPath {
+    [CmdletBinding()]
+    param ()
+    
+    if ($PSVersionTable.Platform -eq 'Unix') {
+        return Join-Path $env:HOME '~/PowerShellAI/ChatGPT'
+    }
+    elseif ($env:APPDATA) {
+        return Join-Path $env:APPDATA 'PowerShellAI/ChatGPT'
+    }
+}
+
+function Get-ChatSessionFile {
+    [CmdletBinding()]
+    param (
+        $timeStamp
+    )
+
+    if (-not $timeStamp) {
+        $timeStamp = Get-ChatSessionTimeStamp
+    }
+
+    Join-Path (Get-ChatSessionPath) ("{0}-ChatGPTSession.xml" -f $timeStamp)
+}
---
+++ __tests__/SessionManagement.tests.ps1
@@ -0,0 +1,78 @@
+Import-Module "$PSScriptRoot\..\PowerShellAI.psd1" -Force
+
+Describe "Session Management" -Tag SessionManagement {
+    BeforeAll {
+        function Test-ChatSessionTimeStamp {
+            param(
+                [Parameter(Mandatory)]
+                [string]$expected
+            )
+
+            ($expected.Length -eq 14) -and ($expected -match "^[0-9]+$")
+        }
+    }
+
+    It 'Test Get-ChatSessionTimeStamp function exists' {
+        $actual = Get-Command Get-ChatSessionTimeStamp -ErrorAction SilentlyContinue
+
+        $actual | Should -Not -BeNullOrEmpty
+    }
+
+    It 'Test Reset-ChatSessionTimeStamp function exists' {
+        $actual = Get-Command Reset-ChatSessionTimeStamp -ErrorAction SilentlyContinue
+
+        $actual | Should -Not -BeNullOrEmpty
+    }
+
+    It 'Test Get-ChatSessionPath function exists' {
+        $actual = Get-Command Get-ChatSessionPath -ErrorAction SilentlyContinue
+
+        $actual | Should -Not -BeNullOrEmpty
+    }
+
+    It 'Test Get-ChatSessionFile function exists' {
+        $actual = Get-Command Get-ChatSessionFile -ErrorAction SilentlyContinue
+
+        $actual | Should -Not -BeNullOrEmpty
+    }
+
+    It 'Test Get-ChatSessionTimeStamp returns a string in the correct format' {
+        $actual = Get-ChatSessionTimeStamp
+
+        Test-ChatSessionTimeStamp $actual | Should -BeTrue
+    }
+
+    It 'Test Reset-ChatSessionTimeStamp resets the session timestamp' {
+        $expected = Get-ChatSessionTimeStamp
+        Reset-ChatSessionTimeStamp
+        
+        # need to wait a second to ensure the timestamp is different
+        Start-Sleep 1
+        $actual = Get-ChatSessionTimeStamp
+
+        $actual | Should -Not -Be $expected
+    }
+
+    It 'Test Get-ChatSessionPath returns correct path for Windows' {
+        $actual = Get-ChatSessionPath
+
+        $actual | Should -BeExactly "$env:APPDATA\PowerShellAI\ChatGPT"
+    }
+
+    It 'Test Get-ChatSessionPath returns correct path for Linux' -Skip {
+
+        $actual = Get-ChatSessionPath
+        $actual | Should -BeExactly ($env:HOME + "~/PowerShellAI/ChatGPT")
+    }
+
+    It 'Test Get-ChatSessionFile returns correct file name' {
+        Reset-ChatSessionTimeStamp
+        $timeStamp = Get-ChatSessionTimeStamp
+
+        $actual = Get-ChatSessionFile
+
+        $expected = "$(Get-ChatSessionPath)\$($timeStamp)-ChatGPTSession.xml"
+
+        $actual | Should -BeExactly $expected
+    }
+}
