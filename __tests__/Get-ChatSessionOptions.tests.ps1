588520f0c6e2540b4b47299a5ae7a152d0c0c7cd
dfinke <finked@hotmail.com>
2023-03-26 16:30:20 -0400

    Add params to set

--- __tests__/Get-ChatSessionOptions.tests.ps1
+++ __tests__/Get-ChatSessionOptions.tests.ps1
@@ -2,7 +2,7 @@ Import-Module "$PSScriptRoot\..\PowerShellAI.psd1" -Force
 
 Describe "Get-ChatSessionOptions" -Tag 'Get-ChatSessionOptions' {
 
-    AfterAll {
+    AfterEach {
         Reset-ChatSessionOptions
     }
     
@@ -38,6 +38,36 @@ Describe "Get-ChatSessionOptions" -Tag 'Get-ChatSessionOptions' {
 
         $actual.model | Should -BeExactly 'davinci'
         $actual.temperature | Should -Be 0.0
+        $actual.max_tokens | Should -Be 256
+        $actual.top_p | Should -Be 1.0
+        $actual.frequency_penalty | Should -Be 0
+        $actual.presence_penalty | Should -Be 0
+        $actual.stop | Should -BeNullOrEmpty
+    }
+
+    It 'Test Set-ChatsessionOption max_tokens' {
+        Set-ChatSessionOption -max_tokens 512
+        $actual = Get-ChatSessionOptions
+        
+        $actual | Should -Not -BeNullOrEmpty
+
+        $actual.model | Should -BeExactly 'gpt-4'
+        $actual.temperature | Should -Be 0.0
+        $actual.max_tokens | Should -Be 512
+        $actual.top_p | Should -Be 1.0
+        $actual.frequency_penalty | Should -Be 0
+        $actual.presence_penalty | Should -Be 0
+        $actual.stop | Should -BeNullOrEmpty
+    }
+
+    It 'Test Set-ChatSessionOption temperature' {
+        Set-ChatSessionOption -temperature 0.5
+        $actual = Get-ChatSessionOptions
+        
+        $actual | Should -Not -BeNullOrEmpty
+
+        $actual.model | Should -BeExactly 'gpt-4'
+        $actual.temperature | Should -Be 0.5
         $actual.max_tokens | Should -Be 256
         $actual.top_p | Should -Be 1.0
         $actual.frequency_penalty | Should -Be 0
