82e36405b86ccba89f80bf203dc1824e6ab7380d
dfinke <finked@hotmail.com>
2023-04-02 13:31:38 -0400

    add help

--- Public/Set-AzureOpenAI.ps1
+++ Public/Set-AzureOpenAI.ps1
@@ -1,4 +1,16 @@
 function Set-AzureOpenAI {
+    <#
+        .SYNOPSIS 
+            Sets the Azure OpenAI API endpoint, deployment name, API version, and API key.
+        .DESCRIPTION
+            Sets up Azure OpenAI as the chat API provider. Use `Set-ChatAPIProvider -Provider OpenAI` to point to the public OpenAI
+        .EXAMPLE
+            Set-AzureOpenAI `
+                -Endpoint https://anEndpoint.openai.azure.com/ `
+                -DeploymentName aName `
+                -ApiVersion 2023-03-15-preview `
+                -ApiKey aKey
+    #>
     [CmdletBinding()]
     param (
         [Parameter(Mandatory)]
@@ -16,5 +28,5 @@ function Set-AzureOpenAI {
 
     Set-ChatAzureOpenAIURIOptions @p
     $env:AzureOpenAIKey = $ApiKey
-    Set-ChatAPIProvider -Provider AzureOpenAI    
+    Set-ChatAPIProvider -Provider AzureOpenAI
 }
