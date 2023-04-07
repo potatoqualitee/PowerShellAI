5796421daf69537f93365ddeceea5c4278bfb4fe
SP3269 <25411814+SP3269@users.noreply.github.com>
2023-04-07 13:16:51 +1000

    ~Original prompt

--- Private/CustomReadHost.ps1
+++ Private/CustomReadHost.ps1
@@ -7,13 +7,13 @@ function CustomReadHost {
         CustomReadHost 
     #>
 
-    $Run = New-Object System.Management.Automation.Host.ChoiceDescription '&Run', 'Run the code'    
+    $Yes = New-Object System.Management.Automation.Host.ChoiceDescription '&Yes', 'Yes, run the code'    
     $Explain = New-Object System.Management.Automation.Host.ChoiceDescription '&Explain', 'Explain the code'
     $Copy = New-Object System.Management.Automation.Host.ChoiceDescription '&Copy', 'Copy to clipboard'    
-    $Nothing = New-Object System.Management.Automation.Host.ChoiceDescription '&Nothing', 'Do not run the code'
+    $No = New-Object System.Management.Automation.Host.ChoiceDescription '&No', 'No, do not run the code'
 
     $options = [System.Management.Automation.Host.ChoiceDescription[]]($Run, $Explain, $Copy, $Nothing)
 
-    $message = 'What would you like to do?'
+    $message = 'Run the code? You can also choose additional actions'
     $host.ui.PromptForChoice($null, $message, $options, 3)
 }
