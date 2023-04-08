766ce253b918879591bf64f425aee3986bcdbcf1
dfinke <finked@hotmail.com>
2023-04-08 08:03:00 -0400

    Variables were used in ChoiceDescription, but not set

--- Private/CustomReadHost.ps1
+++ Private/CustomReadHost.ps1
@@ -7,10 +7,10 @@ function CustomReadHost {
         CustomReadHost 
     #>
 
-    $Yes = New-Object System.Management.Automation.Host.ChoiceDescription '&Yes', 'Yes, run the code'    
+    $Run = New-Object System.Management.Automation.Host.ChoiceDescription '&Yes', 'Yes, run the code'    
     $Explain = New-Object System.Management.Automation.Host.ChoiceDescription '&Explain', 'Explain the code'
     $Copy = New-Object System.Management.Automation.Host.ChoiceDescription '&Copy', 'Copy to clipboard'    
-    $No = New-Object System.Management.Automation.Host.ChoiceDescription '&No', 'No, do not run the code'
+    $Nothing = New-Object System.Management.Automation.Host.ChoiceDescription '&No', 'No, do not run the code'
 
     $options = [System.Management.Automation.Host.ChoiceDescription[]]($Run, $Explain, $Copy, $Nothing)
 
