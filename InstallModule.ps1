7ffb61ed1958dc83cf03ec8ce4b832c1acf4a678
dfinke <finked@hotmail.com>
2023-04-17 08:49:23 -0400

    exclude copying .devcontainers to module folder

--- InstallModule.ps1
+++ InstallModule.ps1
@@ -6,5 +6,5 @@ Select-Object -First 1
 $fullPath = Join-Path $fullPath -ChildPath "PowerShellAI"
 
 Push-location $PSScriptRoot
-Robocopy . $fullPath /mir /XD .vscode images .git .github /XF README.md .gitattributes .gitignore install.ps1 InstallModule.ps1 PublishToGallery.ps1
+Robocopy . $fullPath /mir /XD .vscode images .git .github .devcontainer /XF README.md .gitattributes .gitignore install.ps1 InstallModule.ps1 PublishToGallery.ps1
 Pop-Location
