3a5e1837bddd128401c18ee6a94383bcfb6efe34
dfinke <finked@hotmail.com>
2023-05-21 09:55:14 -0400

    Use SharpToken

--- PowerShellAI.psm1
+++ PowerShellAI.psm1
@@ -1,7 +1,6 @@
-﻿Add-Type -Path "$PSScriptRoot\TiktokenSharp.dll"
+﻿Add-Type -Path "$PSScriptRoot\SharpToken.dll"
 
 $Script:OpenAIKey = $null
-
 
 foreach ($directory in @('Public', 'Private')) {
     Get-ChildItem -Path "$PSScriptRoot\$directory\*.ps1" | ForEach-Object { . $_.FullName }
--- Public/Get-NumberOfTokens.ps1
+++ Public/Get-NumberOfTokens.ps1
@@ -1,48 +1,12 @@
 function Get-NumberOfTokens {
     param(
         [Parameter(Mandatory)]
-        $Messages,
-        $Model = "gpt-3.5-turbo"
+        $Messages
     )
 
-    $PBEFileDirectory = "$PSScriptRoot\PBEFileDirectory"
-    
-    if (!(Test-Path $PBEFileDirectory)) {
-        New-Item -ItemType Directory -Path $PBEFileDirectory | Out-Null
-    }
-    
-    [TiktokenSharp.TikToken]::set_PBEFileDirectory("$PSScriptRoot\PBEFileDirectory")
-    
-    try {
-        $encoding = [TiktokenSharp.TikToken]::EncodingForModel($Model)
-    }
-    catch {
-        if ($null -eq $encoding) {
-            Write-Warning "Model [$($Model)] not found. Using cl100k_base encoding."
-            $encoding = [TiktokenSharp.TikToken]::EncodingForModel("cl100k_base")
-        }
-    }    
- 
-    <#
-    Python:
+    # https://github.com/dmitry-brazhenko/SharpToken
 
-    num_tokens_from_messages(messages, model="gpt-3.5-turbo-0301"):
-    num_tokens = 0
-    for message in messages:
-        num_tokens += tokens_per_message
-        for key, value in message.items():
-            num_tokens += len(encoding.encode(value))
-            if key == "name":
-                num_tokens += tokens_per_name
-    num_tokens += 3  # every reply is primed with <|start|>assistant<|message|>
-#>
-
-    # need to update to take and arry of hashtables returned from Get-ChatMessages
-    # may need to keep for text-in text-out approach    
-    $tokenCount = 0
-    foreach ($message in $Messages) {
-        $tokenCount += @($encoding.Encode($message)).Count
-    }
-    $tokenCount += 3 # every reply is primed with <|start|>assistant<|message|>
-    $tokenCount
+    $encoding = [SharpToken.GptEncoding]::GetEncoding("cl100k_base")    
+    $encoding = [SharpToken.GptEncoding]::GetEncodingForModel("gpt-4")
+    $encoding.Encode($Messages).Count
 }
