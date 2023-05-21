d7039fe9a5657f492f8d21824a070eb3682d3b80
dfinke <finked@hotmail.com>
2023-05-21 10:06:19 -0400

    update

--- Public/Get-NumberOfTokens.ps1
+++ Public/Get-NumberOfTokens.ps1
@@ -4,8 +4,6 @@ function Get-NumberOfTokens {
         $Messages
     )
 
-    # https://github.com/dmitry-brazhenko/SharpToken
-
     $encoding = [SharpToken.GptEncoding]::GetEncoding("cl100k_base")    
     $encoding = [SharpToken.GptEncoding]::GetEncodingForModel("gpt-4")
     $encoding.Encode($Messages).Count
