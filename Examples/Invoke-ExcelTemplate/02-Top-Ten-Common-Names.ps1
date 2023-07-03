83162bc046ba9201768290ae36b57d4c6b435b6b
dfinke <finked@hotmail.com>
2023-07-03 12:07:15 -0400

    Add Invoke-ExcelTemplate

---
+++ Examples/Invoke-ExcelTemplate/02-Top-Ten-Common-Names.ps1
@@ -0,0 +1,3 @@
+. ./Invoke-ExcelTemplate.ps1
+
+Invoke-ExcelTemplate 'top 10 common names, include 5 details about it' 
---
+++ Examples/Invoke-ExcelTemplate/03-Agenda-Sales-Kick-Off.ps1
@@ -0,0 +1,3 @@
+. ./Invoke-ExcelTemplate.ps1
+
+Invoke-ExcelTemplate 'Agenda for a one day sales kick off event with session descriptions and status.' 
