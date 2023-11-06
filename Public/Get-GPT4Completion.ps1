1aa61af7a408f499d6f86f780f14c2e8b4815f05
dfinke <finked@hotmail.com>
2023-11-06 16:35:30 -0500

    add new models

--- Public/Get-GPT4Completion.ps1
+++ Public/Get-GPT4Completion.ps1
@@ -140,7 +140,7 @@ function Set-ChatSessionOption {
     #>
     [CmdletBinding()]
     param(
-        [ValidateSet('gpt-4', 'gpt-4-0613', 'gpt-3.5-turbo', 'gpt-3.5-turbo-16k', 'gpt-3.5-turbo-0613')]
+        [ValidateSet('gpt-3.5-turbo-1106', 'gpt-4-1106-preview', 'gpt-4-0613', 'gpt-3.5-turbo', 'gpt-3.5-turbo-16k', 'gpt-3.5-turbo-0613')]        
         $model,
         $max_tokens = 256,
         $temperature = 0,
